f7c69456a663765d213f68f360fe7fe7af311c82
aaratrikafr-jpg <aaratrikafr@gmail.com>
2025-12-31 14:46:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from datetime import date
 def update_streak():
+   streak_count = update_streak()
     today = date.today()
 
     if "last_study_date" not in st.session_state:
